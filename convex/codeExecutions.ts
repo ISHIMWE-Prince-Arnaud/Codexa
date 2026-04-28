3e9031dfa51a9ff55b90dddc6b30abf0456a22e5
ISHIMWE Prince Arnaud <princeishimwe754@gmail.com>
2026-04-28 12:35:03 +0200

    refactor: clean up code execution logic

--- convex/codeExecutions.ts
+++ convex/codeExecutions.ts
@@ -1,5 +1,6 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { api } from "./_generated/api";
 import { paginationOptsValidator } from "convex/server";
 import { checkRateLimit } from "./rateLimit";
 
